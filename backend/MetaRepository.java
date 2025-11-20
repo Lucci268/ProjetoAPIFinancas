807ae8774355bd7c60fea03c974f7eb06390d90a
Lucci268 <lhpc2808@gmail.com>
2025-11-20 17:40:57 -0300

    Create MetaRepository.java

---
+++ backend/MetaRepository.java
@@ -0,0 +1,6 @@
+package com.cashplus.repository;
+
+import com.cashplus.model.Meta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MetaRepository extends JpaRepository<Meta, Long> {}
