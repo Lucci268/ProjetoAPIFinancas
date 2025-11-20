2029772009f0b638e396e8fcacdffbb5d5984062
Lucci268 <lhpc2808@gmail.com>
2025-11-20 13:01:35 -0300

    Create User.java

---
+++ backend/User.java
@@ -0,0 +1,19 @@
+package com.cashplus.model;
+
+public class User {
+    private Long id;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    public User() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+}
