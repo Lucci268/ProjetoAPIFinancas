4cfd631e0de841db57d558223a5be666aad77283
Júlio César <juliocbizarria@gmail.com>
2026-03-27 19:28:47 -0300

    render

---
+++ src/api/WebConfig.java
@@ -0,0 +1,17 @@
+package api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permite que a Vercel acesse o Render
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
