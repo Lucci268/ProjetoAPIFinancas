07615353ba479c848429505d2f5b113b4a62a77b
Júlio César <juliocbizarria@gmail.com>
2026-03-27 19:39:02 -0300

    vercel 1.0

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+  // Se existir a variável na Vercel, usa ela. Se não, usa o localhost:8081
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8081/api',
 });
 
 export default api;
